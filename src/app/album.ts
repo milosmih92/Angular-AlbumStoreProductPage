2f9fefc977c3612d6ca2abfac9423e1ce9eaeb0f
Milos Mihajlovic <milos.mihajlovic@geta.no>
2022-04-26 23:40:47 +0200

    ANG-1: Formatting.

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,6 +1,6 @@
 import { Track } from './track';
 
-export interface Album {
+export interface Album{
     name: string,
     releaseDate: string,
     coverImage: string,
--- src/app/track.ts
+++ src/app/track.ts
@@ -1,4 +1,4 @@
-export interface Track {
+export interface Track{
     trackNumber: number,
     trackName: string,
     trackLength: string,
